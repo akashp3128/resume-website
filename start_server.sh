d4b3568b208c8b6b94cb386c934552d1e1962de1
akashp3128 <akashp3128@gmail.com>
2025-04-10 21:59:38 -0500

    Clean up directory structure for static site deployment

--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Kill any processes using port 8080 (just in case)
-lsof -ti:8080 | xargs kill -9 2>/dev/null
-
-# Start the HTTP server on port 8080
-python3 -m http.server 8080
-
-echo "Server started on http://localhost:8080" 
+# Simple script to start Python HTTP server for the static HTML site
+echo "Starting HTTP server at http://localhost:8000"
+python3 -m http.server 8000 
